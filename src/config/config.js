3cda586f71728f12909c09e579703a5d75a95de2
Terry <tangfb@novasoftware.cn>
2018-05-29 01:23:57 +0000

    some bug fixed

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,14 @@ const fs = require('fs');
 const path = require('path');
 
 const options = {
-  key: fs.readFileSync(path.join(think.ROOT_PATH, 'src/ssl/server.key')),
+  key: fs.readFileSync(path.join(think.ROOT_PATH, 'src/ssl/test_server.key')),
   cert: fs.readFileSync(path.join(think.ROOT_PATH, 'src/ssl/server.crt'))
 };
 
 module.exports = {
   workers: 1,
+  port: 9090,
+  createServer: function (callback) {
+    return https.createServer(options, callback);
+  }
 };
--- src/controller/api.js
+++ src/controller/api.js
@@ -5,7 +5,36 @@ module.exports = class extends Base {
     async __before() {
         const mock = this.mongoose('mock', 'mongoose');
         const url = this.ctx.url;
-        const action = this.ctx.get('SOAPAction');
+        var getPostData = function(ctx){
+            return new Promise((resolve, reject) => {
+                //获取xml格式的数据
+                if (ctx.request.type.indexOf('xml')>-1) {
+                  var data = "";
+                  ctx.req.on("data", chunk => data += chunk);
+                  ctx.req.on("end", () => resolve(data));
+                }
+                //使用koa-bodyparser获取json格式的数据
+                else resolve(ctx.request.body);
+              }
+          )
+        };
+        let a = await getPostData(this.ctx);
+        let parser = new xml2js.Parser();
+        let getJson = function(data){
+           return new Promise(function(resolve,reject){
+                parser.parseString(data, function (err, result) {
+                   if(err){
+                       reject(err);
+                   }else{
+                       resolve(result);
+                   }
+                });
+            });
+        };
+        console.log(a)
+        let c = await getJson(a);
+        console.log(JSON.stringify(c));
+        const action = Object.keys(c['s:Envelope']['s:Body'][0])[1];
         const data = await mock.find({url:url,action:action});
         if(data.length>0){
             const headers= JSON.parse(data[0].resHeaders);
@@ -15,31 +44,17 @@ module.exports = class extends Base {
             this.ctx.response.body = data[0].resContent;
             return false;
         }else{
-            var getPostData = function(ctx){
-                return new Promise((resolve, reject) => {
-                    //获取xml格式的数据
-                    if (ctx.request.type === "‌text/xml") {
-                      var data = "";
-                      ctx.req.on("data", chunk => data += chunk);
-                      ctx.req.on("end", () => resolve(data));
-                    }
-                    //使用koa-bodyparser获取json格式的数据
-                    else resolve(ctx.request.body);
-                  }
-              )
-            };
-            let a = await getPostData(this.ctx);
-            var builder = new xml2js.Builder();
-            var xml = builder.buildObject(a.post);
-            console.log( Buffer.byteLength(xml));
+            // var builder = new xml2js.Builder();
+            // var xml = builder.buildObject(a.post);
+            // console.log( Buffer.byteLength(xml));
             // console.log(xml);
             // this.ctx.req.body = xml;
             // let res = await proxy.web(this.ctx.req, this.ctx.res, {target: 'https://api.bettingpromotion.com/LiveTestAPI/BookmakerAPIService.asmx'})
            let option = {
             // These properties are part of the Fetch Standard
             method: this.ctx.method,
-            headers: Object.assign(this.ctx.headers,{'Content-Length': Buffer.byteLength(xml)}),        // request headers. format is the identical to that accepted by the Headers constructor (see below)
-            body: xml,         // request body. can be null, a string, a Buffer, a Blob, or a Node.js Readable stream
+            headers: Object.assign(this.ctx.headers,{'Content-Length': Buffer.byteLength(a),'Host':'api.bettingpromotion.com'}),        // request headers. format is the identical to that accepted by the Headers constructor (see below)
+            body: a,         // request body. can be null, a string, a Buffer, a Blob, or a Node.js Readable stream
           
             // The following properties are node-fetch extensions
             timeout: 0,         // req/res timeout in ms, it resets on redirect. 0 to disable (OS limit applies)
@@ -47,12 +62,12 @@ module.exports = class extends Base {
           let res =await this.fetch('https://api.bettingpromotion.com/LiveTestAPI/BookmakerAPIService.asmx',option);
           let data = await res;
           const headers = data.headers.raw();
-        //   console.log(headers);
-        //   for (let key in headers){
-        //       console.log(key +'  ' +headers[key][0].toString());
-        //     this.ctx.set(key,headers[key][0]);
-        //   }
-            this.ctx.set('content-type','text/xml; charset=utf-8')
+         console.log(headers);
+          for (let key in headers){
+              console.log(key +'  ' +headers[key][0].toString());
+            this.ctx.set(key,headers[key][0]);
+          }
+            // this.ctx.set('content-type','text/xml; charset=utf-8')
           this.ctx.response.body = await data.text();
           return false;
         }
